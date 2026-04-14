0dfd726a2667d8be6df14ef97ee5435ca91cfb1e
SpikeSpieg-el <112814519+SpikeSpieg-el@users.noreply.github.com>
2026-04-15 00:04:59 +0500

    1

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const AI_MODELS = {
     lmstudio: {
         name: 'LM Studio',
         baseUrl: 'https://weebx.duckdns.org',
-        apiKey: 'sk-lm-BKnxZktu:9mFDEhcWQG5wBamQeV67',
+        apiKey: 'sk-lm-MnP7r7Dl:h2ZafXVv77GKbgKwcH8Q',
         model: 'google/gemma-4-e4b',
         type: 'openai-compatible'
     },
