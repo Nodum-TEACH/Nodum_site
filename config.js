066606a002c15ee3ee3db840462518c1909d57e6
SpikeSpieg-el <112814519+SpikeSpieg-el@users.noreply.github.com>
2026-04-16 07:42:33 +0500

    1

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 // config.js
 const API_CONFIG = {
     // URL вашего сервера (через Caddy)
-    proxyUrl: 'http://nhost.weebx.duckdns.org/v1/chat-proxy',
-    graphqlUrl: 'http://nhost.weebx.duckdns.org/v1/graphql'
+    proxyUrl: 'https://nhost.weebx.duckdns.org/v1/chat-proxy',
+    graphqlUrl: 'https://nhost.weebx.duckdns.org/v1/graphql'
 };
